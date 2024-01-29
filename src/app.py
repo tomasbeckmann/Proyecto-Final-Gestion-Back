c852194e5c5126bf219d76ef01acf253f126ca03
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-01-29 14:39:38 +0000

    avances

--- src/app.py
+++ src/app.py
@@ -14,11 +14,11 @@ from models import db, User
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+#db_url = os.getenv("DATABASE_URL")
+#if db_url is not None:
+   # app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+#else:
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 MIGRATE = Migrate(app, db)
@@ -36,14 +36,25 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#Creating an user
+@app.route('/user', methods=['POST'])
+def creating_user():
+  get_from_body = request.json.get("email")
+  user = User() 
+  user_exist = User.query.filter_by(email=get_from_body).first()
+  if user_exist is not None:
+    return "The User already exist"
+  else:
+    user.name= request.json.get("name")
+    user.last_name= request.json.get("lastname")
+    user.rut=  request.json.get("rut")
+    user.deleted=  request.json.get("deleted") #Verificar si se deja
+    user.email=  request.json.get("email")
+    user.password=  request.json.get("password")
+    user.userrol_id= request.json.get("userrol_id")
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+    return f"The user was created", 201
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
